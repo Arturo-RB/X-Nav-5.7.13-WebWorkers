ab98c7e381b1d74d3a7fd54f7b5ccb2665dc70ec
arturo <larb_90@hotmail.com>
2017-03-22 01:43:43 +0100

    update

--- webworkers.js
+++ webworkers.js
@@ -3,8 +3,7 @@ function buscarPrimos() {
     var worker = new Worker('calc_primos.js');
     worker.postMessage(document.getElementById("prim").value);
     worker.addEventListener("message", function(e){
-        var primelist=e.data;
-        console.log(primelist);
+        var primelist += e.data;
         document.getElementById('result').innerHTML = primelist;
     });
 }
